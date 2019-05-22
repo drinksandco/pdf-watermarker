510fe0f03df40e8928dd6cf93ece39fda5072bea
Eduard Fabra Bori <eduard.fabra@gmail.com>
2019-05-22 23:02:18 +0200

    Modify Position class to be PHP 7.1 compatible and use this PHP version features

--- src/BinaryStash/PdfWatermarker/Position.php
+++ src/BinaryStash/PdfWatermarker/Position.php
@@ -1,65 +1,61 @@
 <?php
+declare(strict_types=1);
 
 namespace BinaryStash\PdfWatermarker;
 
+use InvalidArgumentException;
+use function in_array;
+
 class Position
 {
+    private const TOP_LEFT = 'TopLeft';
+    private const TOP_CENTER = 'TopCenter';
+    private const TOP_RIGHT = 'TopRight';
+    private const MIDDLE_LEFT = 'MiddleLeft';
+    private const MIDDLE_CENTER = 'MiddleCenter';
+    private const MIDDLE_RIGHT = 'MiddleRight';
+    private const BOTTOM_LEFT = 'BottomLeft';
+    private const BOTTOM_CENTER = 'BottomCenter';
+    private const BOTTOM_RIGHT = 'BottomRight';
+
+    /** @var string */
     private $name;
-    private static $options = [
-        'TopLeft', 'TopCenter', 'TopRight',
-        'MiddleLeft', 'MiddleCenter', 'MiddleRight',
-        'BottomLeft', 'BottomCenter', 'BottomRight',
+
+    private const VALID_POSITIONS = [
+        self::TOP_LEFT,
+        self::TOP_CENTER,
+        self::TOP_RIGHT,
+        self::MIDDLE_LEFT,
+        self::MIDDLE_CENTER,
+        self::MIDDLE_RIGHT,
+        self::BOTTOM_LEFT,
+        self::BOTTOM_CENTER,
+        self::BOTTOM_RIGHT
     ];
 
-    /**
-     * @param $name
-     *
-     * @throws \Exception
-     */
-    public function __construct($name)
+    private function __construct(string $name)
     {
-        if (!array_key_exists($name, array_flip(self::$options))) {
-            throw new \Exception('Unsupported position:' . $name);
+        if (!in_array($name, self::VALID_POSITIONS, true))
+        {
+            throw new InvalidArgumentException('Unsupported position:' . $name);
         }
 
         $this->name = $name;
     }
 
     /**
-     * @return string name
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param Position $position
      *
-     * @return bool
-     */
-    public function equals(Position $position)
-    {
-        return ($this->name === $position->getName());
-    }
-
-    /**
-     * @return string name
-     */
-    public function __toString()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param $name
-     * @param $arguments
+     * @param string $position
      *
      * @return Position
      */
-    public static function __callStatic($name, $arguments)
+    public static function fromName(string $position): self
     {
-        return new self($name);
+        return new self($position);
     }
 
+    public function name(): string
+    {
+        return $this->name;
+    }
 }
