95a15fe64ed4aacd885c504c47e7d7664211294a
mangasf <francisco.mangas.79@gmail.com>
2019-05-29 16:21:24 +0200

    Change autoload namespace.

--- src/BinaryStash/PdfWatermarker/FpdiPdfWatermarker.php
+++ src/BinaryStash/PdfWatermarker/FpdiPdfWatermarker.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace BinaryStash\PdfWatermarker;
+namespace Uvinum\PDFWatermark;
 
 
 use setasign\Fpdi\Fpdi;
--- src/BinaryStash/PdfWatermarker/Pdf.php
+++ src/BinaryStash/PdfWatermarker/Pdf.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace BinaryStash\PdfWatermarker;
+namespace Uvinum\PDFWatermark;
 
 class Pdf extends \SplFileObject
 {
